3ecf4846918e7155527143c79b199dfd2a57885f
chj00216 <76515091+chj00216@users.noreply.github.com>
2020-12-23 18:46:56 +0900

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,5 @@
-input.onGesture(Gesture.Shake, function () {
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Heart)
     music.playTone(415, music.beat(BeatFraction.Whole))
     music.playTone(370, music.beat(BeatFraction.Whole))
     music.playTone(330, music.beat(BeatFraction.Double))
